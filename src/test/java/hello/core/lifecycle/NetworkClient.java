34ee0bbde0bcf3b13348eeb46377fb8a27f33e19
yujineeee <jangy100@nate.com>
2021-06-16 01:05:38 +0900

    빈 생명 주기 콜백 > PostConstructor, PreDestory 어노테이션 방식

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -28,11 +31,13 @@ public class NetworkClient {
         System.out.println("close " + url);
     }
 
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("NetworkClient.destroy");
         disconnect();
     }
 
+    @PostConstruct
     public void init() throws Exception {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
